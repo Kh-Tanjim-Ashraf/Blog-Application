9db8b5d38e244c61cd0693422eb017c49abf66d8
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2025-12-21 02:56:19 +0600

    Included the post-detail feature using the 'slug' field

--- post/urls.py
+++ post/urls.py
@@ -5,4 +5,5 @@ app_name = 'PostApp'
 
 urlpatterns = [
     path('', views.postList, name='postList'),
+    path('<slug:slug>/', views.postDetail, name='postDetail'),  # The first 'slug' is the type specified, the 2nd one is the param that need to be same in both the frontend & django function's parameter
 ]
--- post/views.py
+++ post/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.http import HttpResponse
 
 def postList (request):
     posts = Post.objects.all().order_by('-date')
@@ -7,3 +8,10 @@ def postList (request):
         'posts' : posts
     }
     return render(request, 'post/post-list.html', context)
+
+def postDetail (request, slug):
+    post = Post.objects.get(slug=slug)
+    context = {
+        'post': post
+    }
+    return render(request, 'post/post-detail.html', context)
