f99779d606300c256ffbdf3370783856ff8b5af3
Kh-Tanjim-Ashraf <kh.tanjim.ashraf@gmail.com>
2025-12-22 01:11:57 +0600

    Configure to view media files in post-detail page

--- post/models.py
+++ post/models.py
@@ -6,6 +6,7 @@ class Post(models.Model):
     slug = models.SlugField()
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
+    banner = models.ImageField(default="default.png", blank=True)
 
     def __str__(self):
         return self.title
